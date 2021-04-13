ad55b62bfadee13b8b790da3625fda53d6dd0549
DuCeli <61077016+DuCeli@users.noreply.github.com>
2021-04-13 10:53:58 -0500

    再改一下
    记得删注释！

--- 4.可控分频器设计/Top.v
+++ 4.可控分频器设计/Top.v
@@ -5,14 +5,14 @@ reg [15:0] cnt;
 
 
 always@(posedge clk_in)
-if(!sel)		//sel低电平取学号后4位 1778 
+if(!sel)		//sel低电平取学号后4位
 begin				  // 1后面9个0
-	if(cnt < 849) // 1000000000÷学号后4位÷20*20% 高
+	if(cnt < 5625) // 1000000000÷学号后4位÷20*20% 高
 	begin
 		cnt <= cnt+1;
 		clk_out <= 1;
 	end					  
-	else if(cnt < 4244) // 1000000000÷学号后4位÷20
+	else if(cnt < 28120) // 1000000000÷学号后4位÷20
 	begin
 		cnt <= cnt+1;
 		clk_out <= 0;
@@ -20,14 +20,14 @@ begin				  // 1后面9个0
 	else
 		cnt <= 0;
 end
-else		 //sel高电平取学号后5位 11778
+else		 //sel高电平取学号后5位
 begin
-	if(cnt < 5625) // 1000000000÷学号后5位÷20*20% 高
+	if(cnt < 849) // 1000000000÷学号后5位÷20*20% 高
 	begin
 		cnt <= cnt + 1;
 		clk_out <= 1;
 	end
-	else if(cnt < 28120) // 1000000000÷学号后5位÷20
+	else if(cnt < 4244) // 1000000000÷学号后5位÷20
 	begin
 		cnt <= cnt + 1;
 		clk_out <= 0;
