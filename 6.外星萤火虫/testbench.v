66ca17d62fa4ea3fafe0c1a057828f90d4c116a9
DuCeli <61077016+DuCeli@users.noreply.github.com>
2021-05-11 17:01:48 +0800

    删掉部分注释

--- 6.外星萤火虫/testbench.v
+++ 6.外星萤火虫/testbench.v
@@ -7,12 +7,6 @@ module testbench();		//信号声明
 	initial		//复位
 	begin
 		clk = 0;
-		/*p 	 = 0;
-		sta = 0;
-		#10000000	//10ms
-		p   = 1;
-		#5000000	//5ms
-		sta = 1;*/
 	end
 	initial
 	begin//20组f0,f0周期1ms，频率1000Hz
